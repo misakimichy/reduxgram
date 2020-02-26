d3f16b030308aec21146abf5baa3533af7660d3b
Misaki Koonce <misaki.koonce@gmail.com>
2020-02-26 14:02:34 -0800

    Add Comments component in Post component.

---
+++ client/components/Comments.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Comments extends Component {
+    render() {
+        return (
+        <div className='comment'>
+        comment!
+        </div>
+        )
+    }
+}
+
+export default Comments
--- client/components/Post.js
+++ client/components/Post.js
@@ -1,12 +1,22 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Link } from 'react-router'
+import Photo from './Photo'
+import Comments from './Comments'
 
-const Post = () => {
-    return(
-        <div>
-           Post component!!
-        </div>
-    )
+class Post extends Component {
+
+    render() {
+        // Find index of the post
+        const i = this.props.posts.findIndex(post => post.code === this.props.params.postId)
+        // get us the post
+        const post = this.props.posts[i]
+        return(
+            <div className='single-photo'>
+                <Photo i={i} post={post} {...this.props} />
+                <Comments />
+            </div>
+        )
+    }
 }
 
 export default Post
