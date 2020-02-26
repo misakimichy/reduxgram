ecc61771a546c19a6045a70ee6496f749d61a29f
Misaki Koonce <misaki.koonce@gmail.com>
2020-02-26 08:46:18 -0800

    Add Provider and import history and store to root component.

--- client/data/comments.js
+++ client/data/comments.js
@@ -7,7 +7,7 @@ const comments = {
   ],
   "BAcyDyQwcXX":[
     {
-      "text":"Wes. WE should have lunch.",
+      "text":"Misakimichy. WE should have lunch.",
       "user": "jdaveknox"
     },
     {
@@ -16,7 +16,7 @@ const comments = {
     },
     {
       "text":"@jdaveknox yes!",
-      "user": "wesbos"
+      "user": "misakimichy"
     },
     {
       "text":"😍 love Hamilton!",
@@ -37,7 +37,7 @@ const comments = {
       "user": "henrihelvetica"
     },
     {
-      "text":"@wesbos 👍 my daughter is a pancake eating machine!",
+      "text":"@misakimichy 👍 my daughter is a pancake eating machine!",
       "user": "brentoage"
     },
     {
@@ -50,11 +50,11 @@ const comments = {
     },
     {
       "text":"@gobananna I just made a batch and put in in a FIFO bottle. Keeps in the fridge for a few days.",
-      "user": "wesbos"
+      "user": "misakimichy"
     },
     {
       "text":"@haaps thanks! It's a Jenn air - so nice to cool with!",
-      "user": "wesbos"
+      "user": "misakimichy"
     },
     {
       "text":"Where would you go and for how long, if you had location freedom? - Greg 🌎",
@@ -63,7 +63,7 @@ const comments = {
   ],
   "BAF_KY4wcRY":[
     {
-      "text":"Looking great Wes! I'd like to see the other side of the room too.",
+      "text":"Looking great misakimichy! I'd like to see the other side of the room too.",
       "user": "axcdnt"
     },
     {
@@ -80,19 +80,19 @@ const comments = {
     },
     {
       "text":"@axcdnt that is where I put all the junk. I'll have to clean that side too @henrihelvetica no podcast yet! @pmgllc ohh yeah! @alexbaumgertner yep - the main one is 4K - I'm loving it",
-      "user": "wesbos"
+      "user": "misakimichy"
     },
     {
       "text":"That chrome pillow. 😉",
       "user": "imagesofthisandthat"
     },
     {
-      "text":"@wesbos is that the Dell 4k? The MacBook Pro powers it well? I also have a Retina™ / x1 setup as well. Very handy.",
+      "text":"@misakimichy is that the Dell 4k? The MacBook Pro powers it well? I also have a Retina™ / x1 setup as well. Very handy.",
       "user": "henrihelvetica"
     },
     {
       "text":"#minimalsetups",
-      "user": "wesbos"
+      "user": "misakimichy"
     }
   ],
   "_4jHytwcUA":[
@@ -138,7 +138,7 @@ const comments = {
     },
     {
       "text":"@tjholowaychuk2 haha yes! Old school stylez!",
-      "user": "wesbos"
+      "user": "misakimichy"
     }
   ],
   "_KnU7MwceA":[
@@ -198,11 +198,11 @@ const comments = {
   ],
   "hZh6IQcfN":[
     {
-      "text":"What do we gotta do to get some :)? @wesbos",
+      "text":"What do we gotta do to get some :)? @misakimichy",
       "user": "jonasbad"
     },
     {
-      "text":"Might drop by today - real quick. Trade you an illegal pin for these? Lol. @wesbos",
+      "text":"Might drop by today - real quick. Trade you an illegal pin for these? Lol. @misakimichy",
       "user": "henrihelvetica"
     },
     {
@@ -211,7 +211,7 @@ const comments = {
     },
     {
       "text":"@jonasbad @datamoshr I'll post them up on my blog soon!",
-      "user": "wesbos"
+      "user": "misakimichy"
     },
     {
       "text":"Want",
@@ -285,7 +285,7 @@ const comments = {
     },
     {
       "text":"@br11x I did a teespring a few months ago - maybe I'll do another one soon",
-      "user": "wesbos"
+      "user": "misakimichy"
     }
   ],
   "B3eiIwcYV":[
--- client/reduxstagram.js
+++ client/reduxstagram.js
@@ -1,18 +1,22 @@
 import React from 'react'
 import { render } from 'react-dom'
-import { Router, Route, IndexRoute, browserHistory } from 'react-router'
+import { Router, Route, IndexRoute } from 'react-router'
+import { Provider } from 'react-redux'
 import css from './styles/style.styl'
 import Main from './components/Main'
 import PhotoGrid from './components/PhotoGrid'
 import Post from './components/Post'
+import store, { history } from './store'
 
 const router = (
-    <Router history={browserHistory}>
-        <Route path='/' component={Main}>
-            <IndexRoute component={PhotoGrid} />
-            <Route path='/view/:postId' component={Post} />
-        </Route>
-    </Router>
+    <Provider store={store}>
+        <Router history={history}>
+            <Route path='/' component={Main}>
+                <IndexRoute component={PhotoGrid} />
+                <Route path='/view/:postId' component={Post} />
+            </Route>
+        </Router>
+    </Provider>
 )
 
 render(router, document.getElementById('root'))
