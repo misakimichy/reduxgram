552ad89361f656a74c62964cc7c1956a71ec6037
Misaki Koonce <misaki.koonce@gmail.com>
2020-02-26 10:49:01 -0800

    Add Photo component and render picture, caption, likes and comments.

--- client/components/PhotoGrid.js
+++ client/components/PhotoGrid.js
@@ -1,12 +1,22 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Link } from 'react-router'
+import Photo from './Photo'
 
-const PhotoGrid = () => {
-    return(
-        <div>
-            Photo Grid!
-        </div>
-    )
+class PhotoGrid extends Component {
+    render() {
+        return (
+            <div className='photo-grid'>
+                {this.props.posts.map((post, index) =>
+                    <Photo
+                        key={index}
+                        index={index}
+                        post={post}
+                        {...this.props}
+                    />
+                )}
+            </div>
+        )
+    }
 }
 
 export default PhotoGrid
