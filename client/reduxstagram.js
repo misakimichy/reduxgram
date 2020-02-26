03283b2baa5b10a09a4ffbdf7f0d32d59a17fd70
Misaki Koonce <misaki.koonce@gmail.com>
2020-02-25 22:07:07 -0800

    Add necessary components with Link to Home header.

--- client/reduxstagram.js
+++ client/reduxstagram.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import { render } from 'react-dom'
-
 import css from './styles/style.styl'
+import Main from './components/Main'
 
 render(
-    <p>Yo yo yo yo</p>, document.getElementById('root')
+    <Main />, document.getElementById('root')
 )
